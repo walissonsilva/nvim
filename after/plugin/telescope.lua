8436cb8f0598f644a57f952402e5c83b78f585a2
Walisson Silva <walissonsilva10@gmail.com>
2023-08-26 01:02:55 -0300

    config: hidden uncessary files from telescope search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,22 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+require('telescope').setup {
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  },
+  defaults = {
+    file_ignore_patterns = { "node_modules", "yarn.lock", ".git" },
+    dynamic_preview_title = true,
+    path_display = { 'smart' },
+  },
+  layout_config = {
+    horizontal = {
+      preview_cutoff = 100,
+      preview_width = 0.6
+    }
+  }
+}
