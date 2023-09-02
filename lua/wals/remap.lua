8584fe6bfcdfd959ad11fb07c10c41ff6935c95c
Walisson Silva <walissonsilva10@gmail.com>
2023-09-02 01:57:33 -0300

    refactor: changes remaps to move line and save all files

--- lua/wals/remap.lua
+++ lua/wals/remap.lua
@@ -5,13 +5,13 @@ local keymap = vim.keymap
 keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 keymap.set('n', '<leader>q', '<cmd>q<cr>')
-keymap.set('n', '<leader>w', '<cmd>w<cr>')
+keymap.set('n', '<leader>w', '<cmd>wa<cr>')
 keymap.set('n', '<leader>x', '<cmd>x<cr>')
 
 keymap.set('n', '<c-a>', 'ggVG')
 
 keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-keymap.set('v', 'H', ":m '<-2<CR>gv=gv")
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
 keymap.set("n", '<C-d>', '<C-d>zz')
 keymap.set("n", '<C-u>', '<C-u>zz')
