b02b52d4a9ee2b60c6a7ae332501163c44064228
Walisson Silva <walissonsilva10@gmail.com>
2023-09-02 22:10:08 -0300

    refactor: switch from default to custom tabline plugin config

--- after/plugin/tabline.lua
+++ after/plugin/tabline.lua
@@ -1,1 +1,10 @@
-require("tabline").setup({ opts })
+require('tabline').setup({
+  show_index = false,         -- show tab index
+  show_modify = true,         -- show buffer modification indicator
+  show_icon = true,           -- show file extension icon
+  fnamemodify = ':t',         -- file name modifier
+  modify_indicator = ' *',    -- modify indicator
+  no_name = 'No name',        -- no name buffer name
+  brackets = { '', '' },      -- file name brackets surrounding
+  inactive_tab_max_length = 0 -- max length of inactive tab titles, 0 to ignore
+})
