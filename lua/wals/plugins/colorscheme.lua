e3e757432ec13e3783d923ee7eba32eb13c20036
Walisson Silva <walissonsilva10@gmail.com>
2023-09-12 22:03:20 -0300

    feat: install kanagawa theme

--- lua/wals/plugins/colorscheme.lua
+++ lua/wals/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		-- require("tokyonight").setup({
-		--   style = "storm",
-		--   transparent = true,
-		--   styles = {
-		--     sidebars = "transparent",
-		--     floats = "transparent",
-		--     show_tab_indicators = true,
-		--   },
-		-- })
-		vim.cmd([[colorscheme tokyonight-night]])
-	end,
+  "rebelot/kanagawa.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- require("tokyonight").setup({
+    --   style = "storm",
+    --   transparent = true,
+    --   styles = {
+    --     sidebars = "transparent",
+    --     floats = "transparent",
+    --     show_tab_indicators = true,
+    --   },
+    -- })
+    vim.cmd([[colorscheme kanagawa-dragon]])
+  end,
 }
