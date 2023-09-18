54cfeeb6d074e3e2322ece2d9e4bc436a831d49f
Walisson Silva <walissonsilva10@gmail.com>
2023-09-18 09:20:13 -0300

    feat(colorscheme): update color theme

--- lua/wals/plugins/colorscheme.lua
+++ lua/wals/plugins/colorscheme.lua
@@ -1,29 +1,17 @@
 return {
-  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-  {
-    "baliestri/aura-theme",
-    lazy = false,
-    priority = 1000,
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      vim.cmd([[colorscheme aura-dark]])
-    end
-  }
+  "catppuccin/nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- require("tokyonight").setup({
+    --   style = "storm",
+    --   transparent = true,
+    --   styles = {
+    --     sidebars = "transparent",
+    --     floats = "transparent",
+    --     show_tab_indicators = true,
+    --   },
+    -- })
+    vim.cmd([[colorscheme catppuccin]])
+  end,
 }
--- return {
--- 	"catppuccin/nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		-- require("tokyonight").setup({
--- 		--   style = "storm",
--- 		--   transparent = true,
--- 		--   styles = {
--- 		--     sidebars = "transparent",
--- 		--     floats = "transparent",
--- 		--     show_tab_indicators = true,
--- 		--   },
--- 		-- })
--- 		vim.cmd([[colorscheme catppuccin]])
--- 	end,
--- }
