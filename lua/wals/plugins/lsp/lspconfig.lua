03faff0ebbdca606ce478dd31cae1544f3528b11
Walisson Silva <walissonsilva10@gmail.com>
2023-10-05 17:36:11 -0300

    feat: add support to c# and .net in lsp

--- lua/wals/plugins/lsp/lspconfig.lua
+++ lua/wals/plugins/lsp/lspconfig.lua
@@ -200,6 +200,11 @@ return {
       on_attach = on_attach,
     })
 
+    lspconfig["csharp_ls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
     -- configure lua server (with special settings)
     lspconfig["lua_ls"].setup({
       capabilities = capabilities,
