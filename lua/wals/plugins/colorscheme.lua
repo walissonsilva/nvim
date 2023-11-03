85d94cbe1917de589b7c947b5ae2577fd3a3be3d
Walisson Silva <walissonsilva.dev@gmail.com>
2023-11-02 23:08:53 -0300

    feat: update colorschema

--- lua/wals/plugins/colorscheme.lua
+++ lua/wals/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "storm",
-      transparent = true,  -- disables setting the background color.
-      terminal_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-      sidebars = { "qf", "help" },
-    })
-    vim.cmd([[colorscheme tokyonight-storm]])
-  end,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			style = "storm",
+			transparent = false, -- disables setting the background color.
+			terminal_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			sidebars = { "qf", "help" },
+		})
+		vim.cmd([[colorscheme tokyonight-storm]])
+	end,
 }
